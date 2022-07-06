c72adc42437eeefdca24ee8d1e73808b9e08eeb1
Rayane Ventura <rayyventura@gmail.com>
2022-07-07 01:46:36 +0200

    adds: initial setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+const withImages = require('next-images')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
+
+module.exports = withImages({
+  esModule: true,
+})
+
 
 module.exports = nextConfig
---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Filmhub Music</title>
+        <link rel="shortcut icon" href="/afavicon.png" type="image/x-icon" />
+      </Head>
+
+      <main>
+        <h1>Ola</h1>
+      </main>
+
+      <footer>Ola</footer>
+
+    </div>
+  );
+}
